60473eec9be188bb1bf4a97b271dbe0d45f8496b
Yujie Liu <yujie.liu.home@gmail.com>
2022-06-23 10:28:41 +0200

    [fix] Wrong input for tools

--- src/main/java/nl/utwente/proverb/r2pconvertor/RDF2ProVerB.java
+++ src/main/java/nl/utwente/proverb/r2pconvertor/RDF2ProVerB.java
@@ -24,8 +24,8 @@ public class RDF2ProVerB {
         var model = loadModelFile();
         Convertor convertor;
 
-        //var tools = loadTools();
-        var tools = loadDemo();
+        var tools = loadTools();
+        //var tools = loadDemo();
         for (File tool : tools){
             convertor = new Tool2Tool.Builder(model, tool)
                     .loadRelatedPapers(true)
